a73c436312b07d5830a013f517295d4f4449a4db
Rafed Ramzi <rafedramzi@gmail.com>
2020-11-18 15:36:21 +0700

    Doom - refresh config

--- emacs-doom/.doom.d/config.el
+++ emacs-doom/.doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "chocopowwwa"
-      user-mail-address "chocopowwwa@gmail.com")
+(setq user-full-name "Rafed Ramzi"
+      user-mail-address "rafedramzi@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -19,6 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 ;; (setq doom-font (font-spec :family "Hasklig" :size 18))
 (setq doom-font (font-spec :family "Cascadia Code" :size 18))
 ;; (setq doom-font (font-spec :family "Victor Mono" :size 18))
@@ -27,10 +29,12 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
 ;;(setq doom-theme 'doom-vibrant)
 ;;(setq doom-theme 'doom-horizon)
 ;; (setq doom-theme 'doom-monokai-spectrum)
-(setq doom-theme 'doom-dracula)
+;; (setq doom-theme 'doom-dracula)
 ;;(setq doom-theme 'doom-henna)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -45,7 +49,7 @@
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
+;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
@@ -53,76 +57,8 @@
 ;; - `map!' for binding new keys
 ;;
 ;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
 ;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
-
-;; My Config
-
-;transparent adjustment
-;(set-frame-parameter (selected-frame)'alpha '(95 . 95))
-;(add-to-list 'default-frame-alist'(alpha . (95 . 95)))
-(global-whitespace-mode +1)
-(defvar +cc-default-compiler-options `((c-mode . nil) (c++-mode
-     . ,(list "-std=c++1z" ; use C++17 draft by default
-              (when IS-MAC
-                ;; NOTE beware: you'll get abi-inconsistencies when passing
-                ;; std-objects to libraries linked with libstdc++ (e.g. if you
-                ;; use boost which wasn't compiled with libc++)
-                "-stdlib=libc++")))
-    (objc-mode . nil))
-  "A list of default compiler options for the C family. These are ignored if a
-compilation database is present in the project.")
-
-
-
-;; (treemacs-git-mode 'deferred)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#191919" "#ff5d38" "#98be65" "#bcd42a" "#51afef" "#c678dd" "#46D9FF" "#ede0ce"])
- '(custom-safe-themes
-   (quote
-    ("6177ecbffb8f37756012c9ee9fd73fc043520836d254397566e37c6204118852" "b8929cff63ffc759e436b0f0575d15a8ad7658932f4b2c99415f3dde09b32e97" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "54cf3f8314ce89c4d7e20ae52f7ff0739efb458f4326a2ca075bf34bc0b4f499" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" "9b272154fb77a926f52f2756ed5872877ad8d73d018a426d44c6083d1ed972b1" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" "91375c6dc506913ac7488f655b5afe934f343a0b223021c349105d37748c6696" "1f38fb71e55e5ec5f14a39d03ca7d7a416123d3f0847745c7bade053ca58f043" "34ed3e2fa4a1cb2ce7400c7f1a6c8f12931d8021435bad841fdc1192bd1cc7da" "f66abed5139c808607639e5a5a3b5b50b9db91febeae06f11484a15a92bde442" "ae65ccecdcc9eb29ec29172e1bfb6cadbe68108e1c0334f3ae52414097c501d2" "7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" default)))
- '(fci-rule-color "#5B6268")
- '(jdee-db-active-breakpoint-face-colors (cons "#2b2a27" "#ff5d38"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#2b2a27" "#98be65"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#2b2a27" "#3f444a"))
- '(objed-cursor-color "#ff5d38")
- '(pdf-view-midnight-colors (cons "#ede0ce" "#2b2a27"))
- '(rustic-ansi-faces
-   ["#2b2a27" "#ff5d38" "#98be65" "#bcd42a" "#51afef" "#c678dd" "#46D9FF" "#ede0ce"])
- '(vc-annotate-background "#2b2a27")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#98be65")
-    (cons 40 "#a4c551")
-    (cons 60 "#b0cc3d")
-    (cons 80 "#bcd42a")
-    (cons 100 "#c1a623")
-    (cons 120 "#c5781c")
-    (cons 140 "#cb4b16")
-    (cons 160 "#c95a58")
-    (cons 180 "#c7699a")
-    (cons 200 "#c678dd")
-    (cons 220 "#d96fa6")
-    (cons 240 "#ec666f")
-    (cons 260 "#ff5d38")
-    (cons 280 "#cf563c")
-    (cons 300 "#9f5041")
-    (cons 320 "#6f4a45")
-    (cons 340 "#5B6268")
-    (cons 360 "#5B6268")))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
