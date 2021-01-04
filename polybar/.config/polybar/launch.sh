482fe87ad47107c503ac6002302a3279b32806a1
Rafed Ramzi <rafedramzi@gmail.com>
2021-01-04 11:22:24 +0700

    Polybar - Add alternate name for laptop MONITOR

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+set +e
 ## Add this to your wm startup file.
 
 # Terminate already running bar instances
@@ -9,5 +10,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config top &
+MONITOR=eDP polybar -c ~/.config/polybar/config top &
+MONITOR=eDP-1-0 polybar -c ~/.config/polybar/config top &
 MONITOR=HDMI-0 polybar -c ~/.config/polybar/config top &
